ae11c9c5bcbe9181cd207231840b8374e07fcc43
huangyuan <huangyuan@chunyu.me>
2019-07-26 22:59:55 +0800

    appbar

--- lib/all_widget/bar/app_bar_widget.dart
+++ lib/all_widget/bar/app_bar_widget.dart
@@ -17,24 +17,61 @@ class _AppBarWidgetState extends State<AppBarWidget> {
             child: Icon(Icons.arrow_back)),
         title: Text("title widget"),
         actions: [
-          Icon(Icons.print),
-          Icon(Icons.alarm),
-          Icon(Icons.build),
-          Icon(Icons.adjust)
+          GestureDetector(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Icon(Icons.build),
+            ),
+            onTap: () {
+              print("build");
+              Scaffold.of(context)
+                  .showSnackBar(SnackBar(content: Text("build")));
+            },
+          ),
+          RaisedButton(
+            onPressed: () {
+              Scaffold.of(context)
+                  .showSnackBar(SnackBar(content: Text("add_a_photo")));
+            },
+            child: Icon(Icons.add_a_photo),
+          ),
+          TestSnackBar(),
         ],
         flexibleSpace: Container(
-          height: 100,
           color: Colors.red,
         ),
         elevation: 10.0,
-        shape: CircleBorder(),
         backgroundColor: Colors.amber,
       ),
       bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.android),title: Text("android")),
-        BottomNavigationBarItem(icon: Icon(Icons.done),title: Text("done")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.android), title: Text("android")),
+        BottomNavigationBarItem(icon: Icon(Icons.done), title: Text("done")),
       ]),
       bottomSheet: Text("bottom sheet"),
+      body: GestureDetector(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Icon(Icons.delete),
+        ),
+        onTap: () {
+          print("delete");
+          Scaffold.of(this.context)
+              .showSnackBar(SnackBar(content: Text("delete")));
+        },
+      ),
+    );
+  }
+}
+
+class TestSnackBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text("delete")));
+      },
+      icon: Icon(Icons.done),
     );
   }
 }
